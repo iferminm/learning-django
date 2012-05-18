996482118949759759720d235839b849a447fa39
Israel Fermín Montilla <iferminm@gmail.com>
2012-05-18 17:19:47 -0430

    created some views

--- learningapp/views.py
+++ learningapp/views.py
@@ -1,1 +1,13 @@
-# Create your views here.
+from models import *
+
+def show_professor_profile(request):
+    pass
+
+def show_student_profile(request):
+    pass
+
+def show_course_page(request):
+    pass
+
+def show_general_course_page(request):
+    pass
